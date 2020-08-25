b0dd074428993489484b76bb67f5346f77b513bf
conanalif93 <conanalif93@gmail.com>
2020-08-25 18:50:36 +0800

    Add files via upload

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.mEU7Qj4mXqONg1wDu5xcfgrCKL5Z-qqi);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
