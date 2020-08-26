b2851280a431e79f2abd8ebbec713565dd736d40
conanalif93 <conanalif93@gmail.com>
2020-08-26 15:02:42 +0800

    Adding role funtion

--- bot.js
+++ bot.js
@@ -22,6 +22,41 @@ client.on('message', message => {
 
 });
 
+client.on('messageReactionAdd', (reaction, user) => {
+  const { name } = reaction.emoji;
+  const member = reaction.message.guild.members.cache.get(user.id);
+  if (reaction.message.id === '748054494058774549') {
+    switch (name) {
+      case '<:zeroaiHi:730530086138151024>':
+        member.roles.add('748073452077842523');
+        break;
+      case '<:zeroaiAi:732357048213438577>':
+        member.roles.add('748073680793239572');
+        break;
+      case '<:zeroaiTech:743606222963474542>':
+        member.roles.add('738664618511171634');
+        break;
+    }
+  }
+});
+
+client.on('messageReactionRemove', (reaction, user) => {
+  const { name } = reaction.emoji;
+  const member = reaction.message.guild.members.cache.get(user.id);
+  if (reaction.message.id === '748054494058774549') {
+    switch (name) {
+      case '<:zeroaiHi:730530086138151024>':
+        member.roles.remove('748073452077842523');
+        break;
+      case '<:zeroaiAi:732357048213438577>':
+        member.roles.remove('748073680793239572');
+        break;
+      case '<:zeroaiTech:743606222963474542>':
+        member.roles.remove('738664618511171634');
+        break;
+    }
+  }
+});
  
 
 // THIS  MUST  BE  THIS  WAY
